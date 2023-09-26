6aef9238ea4413547fcaa684e848385229c8b58d
fipezoGithub <fipezocare@gmail.com>
2023-09-26 18:40:16 +0530

    picture upload issue fix

--- client/components/Categories.jsx
+++ client/components/Categories.jsx
@@ -34,7 +34,11 @@ export default function Categories() {
   return (
     <div className={styles.categories}>
       {final.slice(0, 5).map((item, index) => (
-        <Link href={`/explore/freelancer/${item}`} className={styles.category}>
+        <Link
+          href={`/explore/freelancer/${item}`}
+          className={styles.category}
+          key={index}
+        >
           {item === "photographer" && (
             <Image
               src="/photographer-cat.png"
@@ -197,59 +201,6 @@ export default function Categories() {
           {item === "model" && <p className={styles.minText}>Model</p>}
         </Link>
       ))}
-      {/* <Link href="/explore/freelancer/photographer" className={styles.category}>
-        <Image
-          src="/photographerS.webp"
-          width={75}
-          height={75}
-          className={styles.image}
-          alt="photographer logo"
-        />
-        <p className={styles.minText}>Photographer</p>
-      </Link>
-      <Link
-        href="/explore/freelancer/cinematographer"
-        className={styles.category}
-      >
-        <Image
-          src="/cinematographerrS.webp"
-          width={72}
-          height={72}
-          className={styles.image}
-          alt="cinematographer logo"
-        />
-        <p className={styles.minText}>Cinematographer</p>
-      </Link>
-      <Link href="/explore/freelancer/model" className={styles.category}>
-        <Image
-          src="/model-illus.png"
-          width={90}
-          height={90}
-          className={styles.image}
-          alt="model logo"
-        />
-        <p className={styles.minText}>Model</p>
-      </Link>
-      <Link href="/explore/freelancer/photo_editor" className={styles.category}>
-        <Image
-          src="/photo_editor.png"
-          width={90}
-          height={90}
-          className={styles.image}
-          alt="drone logo"
-        />
-        <p className={styles.minText}>Photo Editor</p>
-      </Link>
-      <Link href="/explore/freelancer/video_editor" className={styles.category}>
-        <Image
-          src="/video_editor.png"
-          width={90}
-          height={90}
-          className={styles.image}
-          alt="drone logo"
-        />
-        <p className={styles.minText}>Video Editor</p>
-      </Link> */}
     </div>
   );
 }
